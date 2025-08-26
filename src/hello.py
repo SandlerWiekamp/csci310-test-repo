d173da0d13cb1c290195e5f4004e19527d493f05
SandlerWiekamp <smw015@morningside.edu>
2025-08-26 13:52:29 -0500

    Updated helllo

--- src/hello.py
+++ src/hello.py
@@ -3,6 +3,7 @@ docTest.py
 ====================================
 The core module of my example project
 copied from https://eikonomega.medium.com/getting-started-with-sphinx-autodoc-part-1-2cebbbca5365
+
 | Author: Sandler Wiekamp
 | Date: 2025 August 24
 """ 
